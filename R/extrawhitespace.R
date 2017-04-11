0e769b3e32c52a614b1fcde5080a798f73a0ce54
ErikSchutte <schutte.erik@hotmail.com>
2017-04-11 14:35:18 +0200

    removed prints

--- R/extrawhitespace.R
+++ R/extrawhitespace.R
@@ -8,9 +8,6 @@ extrawhitespace <- function() {
   context <- rstudioapi::getActiveDocumentContext()
   start_line <- context$selection[[1]]$range$start[[1]]
   start_char <- context$selection[[1]]$range$start[[2]]
-  print(start_line)
-  print(start_char)
   insertText("(  )")
   setCursorPosition(c(start_line, start_char+2), id = NULL)
-
 }
