21d42ddd66e2a74e83439f9ee066a261729d90d3
ErikSchutte <schutte.erik@hotmail.com>
2017-04-11 13:45:47 +0200

    Second try..

--- R/extrawhitespace.R
+++ R/extrawhitespace.R
@@ -1,9 +1,9 @@
-#' parenthesesWhiteSpace
+#' extrawhitespace
 #'
 #' Adds a whitespace after parentheses for readability.
 #'
 #' @importFrom rstudioapi insertText
 #' @export
-parenthesesWhiteSpace <- function() {
+extrawhitespace <- function() {
   rstudioapi::insertText(" ")
 }
